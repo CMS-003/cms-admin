e9349a8bc0dc388cfa4c917771a3cb25319fb2d8
阮家友 <1439120442@qq.com>
2022-07-18 16:52:06 +0800

    ~

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ import storage from '../storage';
 import events from '../utils/events';
 
 let options = {
-  SDKAppID: 0 // 接入时需要将 0 替换为您的云通信应用的 SDKAppID，类型为 Number
+  SDKAppID: 1400701118 // 接入时需要将 0 替换为您的云通信应用的 SDKAppID，类型为 Number
 };
 // 创建 SDK 实例，`TIM.create()`方法对于同一个 `SDKAppID` 只会返回同一份实例
 let tim = TIM.create(options); // SDK 实例通常用 tim 表示
