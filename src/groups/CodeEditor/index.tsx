7231ae30e2e0453c6398e83a4949eb05257a8e19
max <ruanjiayou123@gmail>
2025-07-19 21:53:37 +0800

    修改代码编辑器

--- src/groups/CodeEditor/index.tsx
+++ src/groups/CodeEditor/index.tsx
@@ -21,27 +21,27 @@ export default function Editor({ self, mode, drag, dnd, source, setDataField, ch
       {...dnd?.props}
       style={{
         ...dnd?.style,
-        overflowX: 'auto',
-        whiteSpace: 'nowrap',
-        position: 'absolute',
         width: '100%',
+        flex: 0,
         backgroundColor: dnd?.isDragging ? 'lightblue' : '',
       }}
     >
       {children}
-      <div style={{ width: '100%', }}>
-        <CodeMirror
-          key={self._id}
-          value={source[self.widget.field]}
-          className="code-mirror"
-          theme={vscodeDark}
-          style={{ border: '1px dotted grey' }}
-          extensions={[javascript()]}
-          onChange={(value, options) => {
-            setDataField(self.widget, value)
-          }}
-        />
-      </div>
+      <CodeMirror
+        key={self._id}
+        value={source[self.widget.field]}
+        className="code-mirror"
+        theme={vscodeDark}
+        style={{
+          border: '1px dotted grey',
+          width: 700,
+          maxHeight: 400,
+        }}
+        extensions={[javascript()]}
+        onChange={(value, options) => {
+          setDataField(self.widget, value)
+        }}
+      />
     </ComponentWrap>
   )}</Observer>
 }
