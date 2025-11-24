b0a2d6c782d262610023d05fb47593b4524587ea
max <ruanjiayou123@gmail>
2025-11-24 11:13:09 +0800

    回车搜索

--- src/groups/Input/index.tsx
+++ src/groups/Input/index.tsx
@@ -1,12 +1,20 @@
 import CONST from '@/constant';
 import { IAuto, IBaseComponent } from '@/types/component'
 import { Input } from 'antd'
-import { Observer } from 'mobx-react'
+import { Observer, useLocalObservable } from 'mobx-react'
 import { usePageContext } from '../context';
 import { ComponentWrap } from '../style';
+import events from '@/utils/event';
+import { pick } from 'lodash';
 
 export default function CInput({ self, mode, source = {}, drag, dnd, setDataField, children }: IAuto & IBaseComponent) {
   const page = usePageContext();
+  const local = useLocalObservable(() => ({
+    composing: false,
+    setComposition(is: boolean) {
+      local.composing = is;
+    }
+  }))
   return <Observer>{() => (
     <ComponentWrap
       className={mode + drag.className}
@@ -20,9 +28,33 @@ export default function CInput({ self, mode, source = {}, drag, dnd, setDataFiel
       }}
     >
       {children}
-      <Input addonBefore={self.title || null} value={source[self.widget.field]} style={self.style} onChange={e => {
-        setDataField(self.widget, e.target.value);
-      }} />
+      <Input
+        addonBefore={self.title || null}
+        value={source[self.widget.field]}
+        style={self.style}
+        onChange={e => {
+          setDataField(self.widget, e.target.value);
+        }}
+        onCompositionStart={() => {
+          local.setComposition(true)
+        }}
+        onCompositionEnd={() => {
+          local.setComposition(false)
+        }}
+        onKeyDown={e => {
+          if (e.key === 'Enter' && !local.composing && self.widget.action === CONST.ACTION_TYPE.SEARCH) {
+            setDataField({
+              field: 'page',
+              type: 'number',
+              value: 1,
+              query: true,
+              action: '',
+              method: '',
+              refer: []
+            }, 1)
+            events.emit(CONST.ACTION_TYPE.SEARCH, { target: pick(page, ['template_id', 'path', 'param', 'query']) })
+          }
+        }} />
     </ComponentWrap>
   )}</Observer>
 }
